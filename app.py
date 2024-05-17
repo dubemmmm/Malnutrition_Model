5fd9a279816da1f0c7068cb0edcb5a129f461283
Chidubem Onwuchuluba <101866658+dubemmmm@users.noreply.github.com>
2024-05-17 20:19:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ def main():
   st.title('Malnutrition Prediction')
   median_year = st.number_input("MEDIAN YEAR", min_value=1900, max_value=2100, value=2022)
   sample_size = st.slider('SAMPLE SIZE', min_value=0, max_value=15, value=0)
-  severe_wasting = st.slider('SEVERE WASTING', min_value=0, max_value=5, min_values['SEVERE WASTING'])
-  wasting = st.slider('WASTING', min_value=0, max_value=5, min_values['WASTING'])
-  overweight = st.slider('OVERWEIGHT', min_value=0, max_value=5, min_values['OVERWEIGHT'])
-  stunting = st.slider('STUNTING', min_value=0, max_value=5, min_values['STUNTING'])
-  underweight = st.slider('UNDERWEIGHT', min_value=0, max_value=5, min_values['UNDERWEIGHT'])
+  severe_wasting = st.slider('SEVERE WASTING', min_value=0, max_value=5)
+  wasting = st.slider('WASTING', min_value=0, max_value=5)
+  overweight = st.slider('OVERWEIGHT', min_value=0, max_value=5)
+  stunting = st.slider('STUNTING', min_value=0, max_value=5)
+  underweight = st.slider('UNDERWEIGHT', min_value=0, max_value=5)
   country = st.selectbox("COUNTRY", options=["Afghanistan", "Bangladesh", "Cambodia"])
   input_data = {
         'MEDIAN YEAR': [median_year],
